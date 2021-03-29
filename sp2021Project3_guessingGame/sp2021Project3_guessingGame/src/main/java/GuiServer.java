94fdff9a7027614117015355a194858ed6e2abe0
NurAbdel <74681451+NurAbdel@users.noreply.github.com>
2021-03-28 19:12:34 -0500

    New project skeleton
    same as title

---
+++ sp2021Project3_guessingGame/sp2021Project3_guessingGame/src/main/java/GuiServer.java
@@ -0,0 +1,14 @@
+
+import javafx.application.Application;
+
+import javafx.scene.Scene;
+
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GuiServer extends Application  {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
